7484755a34fe9da3dab0ae50b99697d98d814005
HideMe <vahiwe@gmail.com>
2019-10-17 06:32:23 -0400

    pylint tests

--- app.py
+++ app.py
@@ -1,42 +1,51 @@
+""" ChatBot
+"""
 from flask import Flask, render_template, request
 from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer, ListTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer
 from chatterbot.comparisons import levenshtein_distance
 import nltk
 nltk.download('stopwords')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('wordnet')
-app = Flask(__name__)
+APP = Flask(__name__)
 
-english_bot = ChatBot("Chatterbot", 
-                        storage_adapter='chatterbot.storage.MongoDatabaseAdapter',
-                        database_uri='mongodb+srv://root:oyEoPn39je1xGa38@cluster0-bl24x.mongodb.net/test?retryWrites=true&w=majority',
-                        statement_comparison_function=levenshtein_distance,
-                        filters=[
-                        'chatterbot.filters.RepetitiveResponseFilter'
-                        ],
-                        logic_adapters=[
-                        {
-                            'import_path': 'chatterbot.logic.BestMatch',
-                            'threshold': 0.85,
-                            'default_response': 'I am sorry, but I do not understand.'
-                        }])
+ENGLISH_BOT = ChatBot("Chatterbot",
+                      storage_adapter='chatterbot.storage.MongoDatabaseAdapter',
+                      database_uri='mongodb+srv://root:oyEoPn39je1xGa38@cluster0-bl24x.mongodb.net/test?retryWrites=true&w=majority',# pylint: disable=line-too-long
+                      statement_comparison_function=levenshtein_distance,
+                      filters=[
+                          'chatterbot.filters.RepetitiveResponseFilter'],
+                      logic_adapters=[
+                          {
+                              'import_path': 'chatterbot.logic.BestMatch',
+                              'threshold': 0.85,
+                              'default_response': 'I am sorry, but I do not understand.'
+                              }
+                          ]
+                      )
 
-trainer = ChatterBotCorpusTrainer(english_bot)
+TRAINER = ChatterBotCorpusTrainer(ENGLISH_BOT)
 
 # For training the corpus data
-# trainer.train("./data/my_corpus/")
+# TRAINER.train("./data/my_corpus/")
 
 
-@app.route("/")
+@APP.route("/")
 def home():
+    """
+    Home
+    """
     return render_template("index.html")
 
-@app.route("/get")
+@APP.route("/get")
 def get_bot_response():
-    userText = request.args.get('msg')
-    return str(english_bot.get_response(userText))
+    """
+    Get reply from Bot
+    """
+    user_text = request.args.get('msg')
+    return str(ENGLISH_BOT.get_response(user_text))
 
 
 if __name__ == "__main__":
-    app.run()
+    APP.run()
